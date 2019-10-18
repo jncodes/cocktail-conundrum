8c0970f5bb9d9cf8829605429ce3d03881db20ff
jncodes <midn.nelson@gmail.com>
2019-10-18 10:25:08 -0400

    Deploy to GH Pages

--- src/scripts/search_by_name.js
+++ src/scripts/search_by_name.js
@@ -9,7 +9,7 @@ const searchByName = name => {
                 state.push({ 'cocktail': name, 'ingredient': drink.strDrink });
                 for (let i = 1; i <= 15; i++) {
                     if (drink[`strIngredient${i}`] !== null) {
-                        state.push({ 'cocktail': drink.strDrink, 'ingredient': drink[`strIngredient${i}`] });
+                        state.push({ 'cocktail': drink.strDrink, 'ingredient': drink[`strIngredient${i}`], 'image': drink.strDrinkThumb });
                     }
                 }
             });
@@ -42,8 +42,8 @@ const searchByName = name => {
 
             const information = treeStructure(dataStructure);
 
-            // console.log(information.descendants());
-            // console.log(information.links());
+            console.log(information.descendants());
+            console.log(information.links());
             
             const linksGenerator = d3.linkRadial()
                 .angle(function (d) { return d.x / 180 * Math.PI; })
@@ -57,18 +57,68 @@ const searchByName = name => {
                 .style("fill", 'none')
                 .attr("stroke", '#ccc');
             
-            svg.selectAll("g")
+            const nodes = svg.selectAll("g")
                 .data(information.descendants())
                 .enter()
                 .append("g")
                 .attr("transform", function (d) {
                     return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
                 })
-                .append("circle")
-                .attr("r", 7)
-                .style("fill", "#69b3a2")
-                .attr("stroke", "black")
-                .style("stroke-width", 2);
+                
+                nodes.append('image')
+                    .attr('href', function(d) { return d.image; })
+                    .attr("x", "-12px")
+                    .attr("y", "-12px")
+                    .attr("width", "24px")
+                    .attr("height", "24px");
+                debugger
+                // nodes.append("circle")
+                // .attr("r", 7)
+                // .style("fill", "#69b3a2")
+                // .attr("stroke", "black")
+                // .style("stroke-width", 2)
+                
+                nodes.append("text")
+                    // .attr("dy", ".31em")
+                    // .attr("x", function (d) { return d.x < 180 === !d.ingredient ? 6 : -6; })
+                    // .style("text-anchor", function (d) { return d.x < 180 === !d.ingredient ? "start" : "end"; })
+                    // .attr("transform", function (d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
+                    // .text(function (d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
+                    
+                    .text(d => d.data.ingredient)
+                    .classed("text", true)
+                    .attr("font-size", d => (10 + (3 * d.height)))
+                    .attr("x", d => ((d.x < 180) === !d.ingredient ? (d.height + 8) : -(d.height + 8)))
+                    .attr("text-anchor", d => ((d.x < 180) === !d.ingredient ? "start" : "end"))
+                    .attr("transform", d => {
+                        if (d.ingredient) {
+                            return `rotate(${-(d.x - 90)})`
+                        } else {
+                            return (d.x >= 180 ? "rotate(180)" : null)
+                        }
+                    })
+
+                    // .attr("dy", "0.31em")
+                    // .attr("x", d => d.x < Math.PI === !d.ingredient ? 6 : -6)
+                    // .attr("text-anchor", d => d.x < Math.PI === !d.ingredient ? "start" : "end")
+                    // .attr("transform", d => d.x >= Math.PI ? "rotate(180)" : null)
+                    // .text(d => d.data.ingredient)
+                    // .filter(d => d.ingredient)
+                    // .clone(true).lower()
+                    // .attr("stroke", "white");
+            // const names = svg.append('g')
+            //     .selectAll("text")
+            //     .data(information.descendants())
+            //     .enter()
+            //     .append("text")
+            //     // .text(function(d) {return d.data.ingredient;})
+            //     // .attr('x', function(d){return d.x + 5;})
+            //     // .attr('y', function(d){return d.y + 5;})
+            //     .attr("dy", ".31em")
+            //     .attr("x", function (d) { return d.x < 180 === !d.ingredient ? 6 : -6; })
+            //     .style("text-anchor", function (d) { return d.x < 180 === !d.ingredient ? "start" : "end"; })
+            //     .attr("transform", function (d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
+            //     .text(function (d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
 
             // const treeStructure = d3.tree().size([500, 300]);
             // const information = treeStructure(dataStructure);
